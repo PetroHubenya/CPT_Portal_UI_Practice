7d5de680ad3cc43e59f277b484aa525c7b58eb76
Petro Hubenya <p.hubenya@gmail.com>
2024-12-22 16:11:02 -0500

    The "app.routes.ts" file was updated. The route to the login page was created.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './modules/pages/login/login/login.component';
+import { ROUTES } from '../constants/constants';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: ROUTES.LOGIN, component: LoginComponent },
+    // Redirect to security dashboard by default
+    { 
+        path: '', 
+        redirectTo: ROUTES.SECUTITY_DASHBOARD, 
+        pathMatch: 'full'
+    },
+    // Handle invalid routes
+    {
+        path: '**', 
+        redirectTo: ROUTES.SECUTITY_DASHBOARD
+    }
+];
