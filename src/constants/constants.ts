026436cd4bff95b6e578b3775fa75dcccd8204c9
Petro Hubenya <p.hubenya@gmail.com>
2024-12-22 15:53:37 -0500

    constants file was created with routes.

---
+++ src/constants/constants.ts
@@ -0,0 +1,11 @@
+export const ROUTES = {
+    AUTH: 'auth',
+    LOGIN: 'login',
+    HOME: 'home',
+    SECUTITY_DASHBOARD: 'security_dashboard',
+    EXTERNAL_SURFACE_ASSETS: 'external_surface_assets',
+    VULNERABILITIES: 'vulNERabilities',
+    EMAIL_HEALTH: 'email_health',
+    PUBLIC_DATA: 'public_data', // Suspicious urls emails.
+    CUSTOMERS: 'customers', // List of customers' users needed at the admin logging in.
+}
