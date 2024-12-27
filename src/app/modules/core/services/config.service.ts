630a20b828ec92ffb2b730e16a81934a0eedb315
Petro Hubenya <p.hubenya@gmail.com>
2024-12-27 11:05:34 -0500

    The Azure AD configuration settings were added to the constants.

--- src/app/modules/core/services/config.service.ts
+++ src/app/modules/core/services/config.service.ts
@@ -1,8 +1,3 @@
-// The ConfigService is used to manage environment-specific configurations dynamically
-// and provide a centralized way to access these configurations throughout the application.
-// It serves as a flexible and reusable solution, especially when you need to load environment variables
-// or settings at runtime, rather than at build time.
-
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 
@@ -12,10 +7,10 @@ type EnvironmentType = typeof environment;
 @Injectable({
   providedIn: 'root'
 })
+
+// The ConfigService provides a convenient way to access environment-specific configurations
 export class ConfigService {
   private config: EnvironmentType = environment;
-
-  constructor() {}
 
   // Explicitly handle key access with string indexing
   get<T extends keyof EnvironmentType>(key: T): EnvironmentType[T] {
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,6 @@
+import { environment } from '../environments/environment';
+
+// Route configuration.
 export const ROUTES = {
     AUTH: 'auth',
     LOGIN: 'login',
@@ -6,6 +9,18 @@ export const ROUTES = {
     EXTERNAL_SURFACE_ASSETS: 'external_surface_assets',
     VULNERABILITIES: 'vulNERabilities',
     EMAIL_HEALTH: 'email_health',
-    PUBLIC_DATA: 'public_data', // Suspicious urls emails.
-    CUSTOMERS: 'customers', // List of customers' users needed at the admin logging in.
+    PUBLIC_DATA: 'public_data', // Suspicious urls emails page.
+    CUSTOMERS: 'customers', // When admin logs in they are suypposed to select a user, of customer type, to display.
 }
+
+// Azure AD configuration settings.
+export const AUTH_CONFIG = {
+    b2cPoliciesName: { signin: environment.azure.authFLowName },
+    authorities: { signin: environment.azure.authFlowLink },
+    domain: environment.azure.domain,
+    clientID: environment.azure.clientID,
+    redirectUri: '/auth',
+    postLogoutRedirectUri: '/login',
+    protectedResource: environment.apiUrl,
+    scopes: environment.azure.scopes
+};
